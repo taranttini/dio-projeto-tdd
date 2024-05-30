34bb5242c382610d1976b61f2a6ff7895acbb17f
Tarantini Pereira <taranttini@hotmail.com>
2024-05-30 18:18:26 -0300

    criando a estrutura do projeto

---
+++ projeto-tdd/ServicoAgenda.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ServicoAgenda.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ projeto-tdd/ServicoAgenda/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ServicoAgenda;
+
+public class Class1
+{
+}
